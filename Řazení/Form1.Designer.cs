f6ad64fd7b79e1de66cf0e1a4a5f6954a388e306
tensevera <Jan-Severa@seznam.cz>
2020-04-17 16:22:27 +0200

    aa

---
+++ Řazení/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Řazení
+{
+    partial class Řazení
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.velikostPole = new System.Windows.Forms.NumericUpDown();
+            this.tlStart = new System.Windows.Forms.Button();
+            this.vstupniPole = new System.Windows.Forms.DataGridView();
+            this.tlNoveHodnoty = new System.Windows.Forms.Button();
+            this.vystupniPole = new System.Windows.Forms.DataGridView();
+            this.tlDoleSmaz = new System.Windows.Forms.Button();
+            this.tlKopirujDolu = new System.Windows.Forms.Button();
+            this.tlSeradVyberMinima = new System.Windows.Forms.Button();
+            this.bt_bubblesort = new System.Windows.Forms.Button();
+            this.bt_Combsort = new System.Windows.Forms.Button();
+            this.bt_quicksort = new System.Windows.Forms.Button();
+            this.bt_HeapSort = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.velikostPole)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vstupniPole)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vystupniPole)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // velikostPole
+            // 
+            this.velikostPole.Cursor = System.Windows.Forms.Cursors.Default;
+            this.velikostPole.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.velikostPole.Location = new System.Drawing.Point(15, 20);
+            this.velikostPole.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.velikostPole.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.velikostPole.Name = "velikostPole";
+            this.velikostPole.Size = new System.Drawing.Size(40, 29);
+            this.velikostPole.TabIndex = 0;
+            this.velikostPole.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.velikostPole.ValueChanged += new System.EventHandler(this.velikostPole_ValueChanged);
+            // 
+            // tlStart
+            // 
+            this.tlStart.Location = new System.Drawing.Point(95, 20);
+            this.tlStart.Name = "tlStart";
+            this.tlStart.Size = new System.Drawing.Size(164, 29);
+            this.tlStart.TabIndex = 1;
+            this.tlStart.Text = "Start – generuj nové pole!";
+            this.tlStart.UseVisualStyleBackColor = true;
+            this.tlStart.Click += new System.EventHandler(this.tlStart_Click);
+            // 
+            // vstupniPole
+            // 
+            this.vstupniPole.AllowUserToAddRows = false;
+            this.vstupniPole.AllowUserToDeleteRows = false;
+            this.vstupniPole.AllowUserToResizeColumns = false;
+            this.vstupniPole.AllowUserToResizeRows = false;
+            this.vstupniPole.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.vstupniPole.ColumnHeadersVisible = false;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.vstupniPole.DefaultCellStyle = dataGridViewCellStyle5;
+            this.vstupniPole.Location = new System.Drawing.Point(313, 20);
+            this.vstupniPole.MultiSelect = false;
+            this.vstupniPole.Name = "vstupniPole";
+            this.vstupniPole.ReadOnly = true;
+            this.vstupniPole.RowHeadersVisible = false;
+            this.vstupniPole.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.vstupniPole.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.vstupniPole.Size = new System.Drawing.Size(240, 23);
+            this.vstupniPole.TabIndex = 2;
+            this.vstupniPole.Visible = false;
+            this.vstupniPole.SelectionChanged += new System.EventHandler(this.vstupniPole_SelectionChanged);
+            // 
+            // tlNoveHodnoty
+            // 
+            this.tlNoveHodnoty.Location = new System.Drawing.Point(15, 68);
+            this.tlNoveHodnoty.Name = "tlNoveHodnoty";
+            this.tlNoveHodnoty.Size = new System.Drawing.Size(160, 29);
+            this.tlNoveHodnoty.TabIndex = 3;
+            this.tlNoveHodnoty.Text = "Generuj nahoru nové hodnoty!";
+            this.tlNoveHodnoty.UseVisualStyleBackColor = true;
+            this.tlNoveHodnoty.Visible = false;
+            this.tlNoveHodnoty.Click += new System.EventHandler(this.tlNoveHodnoty_Click);
+            // 
+            // vystupniPole
+            // 
+            this.vystupniPole.AllowUserToAddRows = false;
+            this.vystupniPole.AllowUserToDeleteRows = false;
+            this.vystupniPole.AllowUserToResizeColumns = false;
+            this.vystupniPole.AllowUserToResizeRows = false;
+            this.vystupniPole.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.vystupniPole.ColumnHeadersVisible = false;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.vystupniPole.DefaultCellStyle = dataGridViewCellStyle6;
+            this.vystupniPole.Location = new System.Drawing.Point(15, 110);
+            this.vystupniPole.MultiSelect = false;
+            this.vystupniPole.Name = "vystupniPole";
+            this.vystupniPole.ReadOnly = true;
+            this.vystupniPole.RowHeadersVisible = false;
+            this.vystupniPole.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.vystupniPole.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.vystupniPole.Size = new System.Drawing.Size(240, 23);
+            this.vystupniPole.TabIndex = 4;
+            this.vystupniPole.Visible = false;
+            this.vystupniPole.SelectionChanged += new System.EventHandler(this.vystupniPole_SelectionChanged);
+            // 
+            // tlDoleSmaz
+            // 
+            this.tlDoleSmaz.Location = new System.Drawing.Point(181, 68);
+            this.tlDoleSmaz.Name = "tlDoleSmaz";
+            this.tlDoleSmaz.Size = new System.Drawing.Size(74, 29);
+            this.tlDoleSmaz.TabIndex = 5;
+            this.tlDoleSmaz.Text = "Dole smaž!";
+            this.tlDoleSmaz.UseVisualStyleBackColor = true;
+            this.tlDoleSmaz.Visible = false;
+            this.tlDoleSmaz.Click += new System.EventHandler(this.tlDoleSmaz_Click);
+            // 
+            // tlKopirujDolu
+            // 
+            this.tlKopirujDolu.Location = new System.Drawing.Point(261, 68);
+            this.tlKopirujDolu.Name = "tlKopirujDolu";
+            this.tlKopirujDolu.Size = new System.Drawing.Size(78, 29);
+            this.tlKopirujDolu.TabIndex = 6;
+            this.tlKopirujDolu.Text = "Kopíruj dolů!";
+            this.tlKopirujDolu.UseVisualStyleBackColor = true;
+            this.tlKopirujDolu.Visible = false;
+            this.tlKopirujDolu.Click += new System.EventHandler(this.tlKopirujDolu_Click);
+            // 
+            // tlSeradVyberMinima
+            // 
+            this.tlSeradVyberMinima.Location = new System.Drawing.Point(345, 68);
+            this.tlSeradVyberMinima.Name = "tlSeradVyberMinima";
+            this.tlSeradVyberMinima.Size = new System.Drawing.Size(129, 29);
+            this.tlSeradVyberMinima.TabIndex = 7;
+            this.tlSeradVyberMinima.Text = "Seřaď výběrem minima!";
+            this.tlSeradVyberMinima.UseVisualStyleBackColor = true;
+            this.tlSeradVyberMinima.Visible = false;
+            this.tlSeradVyberMinima.Click += new System.EventHandler(this.tlSeradVyberMinima_Click);
+            // 
+            // bt_bubblesort
+            // 
+            this.bt_bubblesort.Location = new System.Drawing.Point(480, 68);
+            this.bt_bubblesort.Name = "bt_bubblesort";
+            this.bt_bubblesort.Size = new System.Drawing.Size(129, 29);
+            this.bt_bubblesort.TabIndex = 8;
+            this.bt_bubblesort.Text = "Seřaď bubble sortem!";
+            this.bt_bubblesort.UseVisualStyleBackColor = true;
+            this.bt_bubblesort.Visible = false;
+            this.bt_bubblesort.Click += new System.EventHandler(this.bt_bubblesort_Click);
+            // 
+            // bt_Combsort
+            // 
+            this.bt_Combsort.Location = new System.Drawing.Point(615, 68);
+            this.bt_Combsort.Name = "bt_Combsort";
+            this.bt_Combsort.Size = new System.Drawing.Size(129, 29);
+            this.bt_Combsort.TabIndex = 9;
+            this.bt_Combsort.Text = "Seřaď comb sortem!";
+            this.bt_Combsort.UseVisualStyleBackColor = true;
+            this.bt_Combsort.Visible = false;
+            this.bt_Combsort.Click += new System.EventHandler(this.bt_Combsort_Click);
+            // 
+            // bt_quicksort
+            // 
+            this.bt_quicksort.Location = new System.Drawing.Point(885, 68);
+            this.bt_quicksort.Name = "bt_quicksort";
+            this.bt_quicksort.Size = new System.Drawing.Size(129, 43);
+            this.bt_quicksort.TabIndex = 10;
+            this.bt_quicksort.Text = "Seřaď quick sortem! //nefunguje";
+            this.bt_quicksort.UseVisualStyleBackColor = true;
+            this.bt_quicksort.Visible = false;
+            this.bt_quicksort.Click += new System.EventHandler(this.bt_quicksort_Click);
+            // 
+            // bt_HeapSort
+            // 
+            this.bt_HeapSort.Location = new System.Drawing.Point(750, 68);
+            this.bt_HeapSort.Name = "bt_HeapSort";
+            this.bt_HeapSort.Size = new System.Drawing.Size(129, 29);
+            this.bt_HeapSort.TabIndex = 11;
+            this.bt_HeapSort.Text = "Seřaď heap sortem!";
+            this.bt_HeapSort.UseVisualStyleBackColor = true;
+            this.bt_HeapSort.Visible = false;
+            this.bt_HeapSort.Click += new System.EventHandler(this.bt_HeapSort_Click);
+            // 
+            // Řazení
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 166);
+            this.Controls.Add(this.bt_HeapSort);
+            this.Controls.Add(this.bt_quicksort);
+            this.Controls.Add(this.bt_Combsort);
+            this.Controls.Add(this.bt_bubblesort);
+            this.Controls.Add(this.tlSeradVyberMinima);
+            this.Controls.Add(this.tlKopirujDolu);
+            this.Controls.Add(this.tlDoleSmaz);
+            this.Controls.Add(this.vystupniPole);
+            this.Controls.Add(this.tlNoveHodnoty);
+            this.Controls.Add(this.vstupniPole);
+            this.Controls.Add(this.tlStart);
+            this.Controls.Add(this.velikostPole);
+            this.Name = "Řazení";
+            this.Text = "Řazení";
+            ((System.ComponentModel.ISupportInitialize)(this.velikostPole)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vstupniPole)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vystupniPole)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown velikostPole;
+        private System.Windows.Forms.Button tlStart;
+        private System.Windows.Forms.DataGridView vstupniPole;
+        private System.Windows.Forms.Button tlNoveHodnoty;
+        private System.Windows.Forms.DataGridView vystupniPole;
+        private System.Windows.Forms.Button tlDoleSmaz;
+        private System.Windows.Forms.Button tlKopirujDolu;
+        private System.Windows.Forms.Button tlSeradVyberMinima;
+        private System.Windows.Forms.Button bt_bubblesort;
+        private System.Windows.Forms.Button bt_Combsort;
+        private System.Windows.Forms.Button bt_quicksort;
+        private System.Windows.Forms.Button bt_HeapSort;
+    }
+}
+
---
+++ Řazení/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Řazení
+{
+    public partial class Řazení : Form
+    {
+        const int maxCislo = 99;        //generujeme číslo od 0 do maxCislo (včetně)
+        int[] Pole;                     //globální pole, které obsahuje vygenerované hodnoty
+        const int velikostBunky = 36;   //velikosti buněk gridu
+        Random r = new Random();
+
+        public Řazení()
+        {
+            InitializeComponent();
+        }
+
+        private void tlStart_Click(object sender, EventArgs e)
+        {
+            Pole = new int[Convert.ToInt32(velikostPole.Value)];    //nastavíme poli správnou délku
+            //pole narandomujeme
+            for (int i = 0; i < Pole.Length; i++)
+                Pole[i] = r.Next(maxCislo + 1);
+
+            //nastavíme vstupní grid
+            vstupniPole.Left = 15;
+            vstupniPole.Visible = true;
+            //nastavíme velikost vstupnímu gridu
+            //nastavíme sloupce a šířku
+            vstupniPole.ColumnCount = Pole.Length;
+            for (int i = 0; i < vstupniPole.ColumnCount; i++)
+            {
+                vstupniPole.Columns[i].Width = velikostBunky;
+            }
+            vstupniPole.Width = vstupniPole.ColumnCount * velikostBunky + 3;
+            //nastavíme řádky a výšku
+            vstupniPole.RowCount = 1;
+            vstupniPole.Rows[0].Height = velikostBunky;
+            vstupniPole.Height = velikostBunky + 3;
+
+            //vypíšeme do vstupního gridu
+            for (int i = 0; i < Pole.Length; i++)
+                vstupniPole.Rows[0].Cells[i].Value = Pole[i];
+
+            //skryjeme 
+            tlStart.Visible = false;
+            velikostPole.Visible = false;
+
+            //zobrazíme skrytá tlačítka
+            tlNoveHodnoty.Visible = true;
+            tlDoleSmaz.Visible = true;
+            tlKopirujDolu.Visible = true;
+            tlSeradVyberMinima.Visible = true;
+            bt_bubblesort.Visible = true;
+            bt_Combsort.Visible = true;
+            bt_quicksort.Visible = true;
+            bt_HeapSort.Visible = true;
+
+            //připravíme vystupni pole
+            vystupniPole.Visible = true;
+            //nastavíme velikost výstupnímu gridu
+            //nastavíme sloupce a šířku
+            vystupniPole.ColumnCount = Pole.Length;
+            for (int i = 0; i < vystupniPole.ColumnCount; i++)
+            {
+                vystupniPole.Columns[i].Width = velikostBunky;
+            }
+            vystupniPole.Width = vystupniPole.ColumnCount * velikostBunky + 3;
+            //nastavíme řádky a výšku
+            vystupniPole.RowCount = 1;
+            vystupniPole.Rows[0].Height = velikostBunky;
+            vystupniPole.Height = velikostBunky + 3;
+        }
+
+        private void vstupniPole_SelectionChanged(object sender, EventArgs e)   //aby nebylo modré označení
+        {
+            vstupniPole.ClearSelection();
+        }
+
+        private void vystupniPole_SelectionChanged(object sender, EventArgs e)
+        {
+            vystupniPole.ClearSelection();
+        }
+
+        private void tlNoveHodnoty_Click(object sender, EventArgs e)
+        {
+            //vygenerujeme nové hodnoty
+            for (int i = 0; i < Pole.Length; i++)
+                Pole[i] = r.Next(maxCislo + 1);
+            //vypíšeme do vstupního gridu
+            for (int i = 0; i < Pole.Length; i++)
+                vstupniPole.Rows[0].Cells[i].Value = Pole[i];
+        }
+
+
+        private void tlDoleSmaz_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Pole.Length; i++)
+                vystupniPole.Rows[0].Cells[i].Value = null;
+        }
+
+        private void tlKopirujDolu_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Pole.Length; i++)
+                vystupniPole.Rows[0].Cells[i].Value = vstupniPole.Rows[0].Cells[i].Value;
+        }
+
+        private void tlSeradVyberMinima_Click(object sender, EventArgs e)
+        {
+            int[] Kopie = new int[Pole.Length];     //vytvořím kopii vstupního pole a to seřadím
+            for (int i = 0; i < Pole.Length; i++)   //nemohu Kopie=Pole - byl by to jen ukazatel; šlo by CopyTo
+                Kopie[i] = Pole[i];
+            //zavolám řazení na Kopii
+            seradVyberMinima(Kopie);            //předávám hodnotou – referenční typ
+            //Kopii vypíši do dolního gridu
+            for (int i = 0; i < Kopie.Length; i++)
+                vystupniPole.Rows[0].Cells[i].Value = Kopie[i];
+        }
+
+        private void seradVyberMinima(int[] P)   //řazením výběrem minima
+        {
+            int posun = 0; //hranice posunu
+            do {
+                int nejmensi = posun;
+                for (int x = posun+1; x < P.Length; x++)
+                {
+                    if (P[x] < P[nejmensi])
+                    {
+                        nejmensi = x;
+                    }
+                }
+                int docas = P[nejmensi];
+                P[nejmensi] = P[posun];
+                P[posun] = docas;
+                posun++;
+            } while (posun < P.Length - 1);
+        }
+
+        private void bt_bubblesort_Click(object sender, EventArgs e) //bubble sort click
+        {
+            int[] Kopie = new int[Pole.Length];     //vytvořím kopii vstupního pole a to seřadím
+            for (int i = 0; i < Pole.Length; i++)   //nemohu Kopie=Pole - byl by to jen ukazatel; šlo by CopyTo
+                Kopie[i] = Pole[i];
+            //zavolám řazení na Kopii
+            seradbubble(Kopie);            //předávám hodnotou – referenční typ
+            //Kopii vypíši do dolního gridu
+            for (int i = 0; i < Kopie.Length; i++)
+                vystupniPole.Rows[0].Cells[i].Value = Kopie[i];
+        }
+        private void seradbubble(int[] P) //BUBBLE SORT
+        {
+            bool serazeno;
+            int konec = P.Length - 1;
+            do
+            {
+                serazeno = true;
+                for (int x = 0; x < konec; x++)
+                {
+                    if (P[x] > P[x + 1])
+                    {
+                        int docas = P[x];
+                        P[x] = P[x + 1];
+                        P[x + 1] = docas;
+                        serazeno = false;
+                    }
+                }
+                konec--;
+            } while (serazeno == false && konec > 0);
+        }
+
+        private void bt_Combsort_Click(object sender, EventArgs e)
+        {
+            int[] Kopie = new int[Pole.Length];     //vytvořím kopii vstupního pole a to seřadím
+            for (int i = 0; i < Pole.Length; i++)   //nemohu Kopie=Pole - byl by to jen ukazatel; šlo by CopyTo
+                Kopie[i] = Pole[i];
+            //zavolám řazení na Kopii
+            seradCombSortem(Kopie);            //předávám hodnotou – referenční typ
+            //Kopii vypíši do dolního gridu
+            for (int i = 0; i < Kopie.Length; i++)
+                vystupniPole.Rows[0].Cells[i].Value = Kopie[i];
+        }
+        private void seradCombSortem(int[] P) //Comb SORT -- OBČAS NEFUNGUJE (víc hodnot)
+        {
+            int mezera = Convert.ToInt32(Math.Floor(P.Length/1.3)); //mezera
+            do
+            {
+                for (int i = 0; i < P.Length - mezera; i++) //pokud aktuální pozice je větší než pozice + mezera
+                {
+                    if (P[i] > P[i+mezera]) //vyměnit
+                    {
+                        int docas = P[i];
+                        P[i] = P[i + mezera];
+                        P[i + mezera] = docas;
+                    }
+                }
+                mezera = Convert.ToInt32(Math.Floor(mezera / 1.3)); //mezera se zmenší
+            } while (mezera >= 1);
+        }
+
+        private void bt_quicksort_Click(object sender, EventArgs e)
+        {
+            int[] Kopie = new int[Pole.Length];     //vytvořím kopii vstupního pole a to seřadím
+            for (int i = 0; i < Pole.Length; i++)   //nemohu Kopie=Pole - byl by to jen ukazatel; šlo by CopyTo
+                Kopie[i] = Pole[i];
+            //zavolám řazení na Kopii
+            seradQuickSortem(Kopie);            //předávám hodnotou – referenční typ
+            //Kopii vypíši do dolního gridu
+            for (int i = 0; i < Kopie.Length; i++)
+                vystupniPole.Rows[0].Cells[i].Value = Kopie[i];
+        }
+        private void seradQuickSortem(int[] P) //QUICKSORT
+        {
+            rozdeleni(P, P.Length - 1, -1); //prvotní určení zlomového bodu                              
+           for (int i = konec; i >= 0; i--)
+           {
+                rozdeleni(P, i+1, -1);
+           }
+           for (int i = konec + 1; i<P.Length-1; i++)
+           {
+                rozdeleni(P, P.Length - 1, i);
+           }
+                                            
+        }
+        int konec;
+        private void rozdeleni(int[] P,int vrchol, int zacatek)
+        {
+            int prostredek = P[vrchol]; //prostřední hodnotu udá jako poslední číslo řady
+            int mensich = zacatek; //počet menších čísel
+            for (int i = zacatek + 1; i < vrchol; i++) //od zadaného bodu do konce řady seřadí pole 
+            {
+                if (P[i] < prostredek)
+                {
+                    mensich++;
+                    int docasc = P[i];
+                    P[i] = P[mensich];
+                    P[mensich] = docasc;
+                }
+            }  
+            int docas = P[vrchol]; //zařazení prostředního bodu
+            P[vrchol] = P[mensich+1];
+            P[mensich + 1] = docas;
+            konec = mensich;
+        }
+
+        private void bt_HeapSort_Click(object sender, EventArgs e)
+        {
+            int[] Kopie = new int[Pole.Length];     //vytvořím kopii vstupního pole a to seřadím
+            for (int i = 0; i < Pole.Length; i++)   //nemohu Kopie=Pole - byl by to jen ukazatel; šlo by CopyTo
+                Kopie[i] = Pole[i];
+            //zavolám řazení na Kopii
+            seradHeapSortem(Kopie);            //předávám hodnotou – referenční typ
+            //Kopii vypíši do dolního gridu
+            for (int i = 0; i < Kopie.Length; i++)
+                vystupniPole.Rows[0].Cells[i].Value = Kopie[i];
+        }
+        private void seradHeapSortem(int[] P)
+        {
+            int n = Convert.ToInt32(velikostPole.Value); //velikost pole
+            for (int i = n / 2 - 1; i >= 0; i--) //vybudujeme strom ( přeházíme pole)
+            {
+                heap(P, n, i);
+            }
+            for ( int i = n -1; i > 0; i--) //dostáváme hodnoty zpět
+            {
+                int mezi = P[0]; //přidáme kořen na konec
+                P[0] = P[i];
+                P[i] = mezi;
+                heap(P, i, 0); //znovu seřadíme
+            }
+            void heap(int[] pole, int x, int y) //seřazení stromu (y = kořen, x = délka)
+            {
+                int největší = y; //kořen
+                int l = 2 * y + 1; //levá
+                int p = 2 * y + 2; //pravá
+                if ( l < x &&  pole[l] > pole[největší]) //levý syn je větší jak kořen
+                {
+                    největší = l;
+                }
+                if (p < x && pole[p] > pole[největší]) //pravý syn je větší jak kořen
+                {
+                    největší = p;
+                }
+                if (největší != y) //kořen není největší
+                {
+                    int mezi = pole[y];
+                    pole[y] = pole[největší];
+                    pole[největší] = mezi;
+                    heap(pole, x, největší); //seřadíme haldu
+                }
+            }
+        }
+
+        private void velikostPole_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
